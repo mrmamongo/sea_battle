086f6449a635b071399f78619e5f9322cd36a999
FOMBUS1 <krivoshapko22artem@gmail.com>
2024-02-23 23:18:58 +0500

    Fixing bugs

--- backend/src/infra/redis/models.py
+++ backend/src/infra/redis/models.py
@@ -2,26 +2,22 @@ from dataclasses import dataclass
 from enum import Enum
 
 class CellState(str, Enum):
-    EMPTY = 'E'
-    SHIP = 'S'
-    HIT = 'H'
-    MISS = 'M'
-    SUNK = 'K'
+    EMPTY = 'empty'
+    SHIP = 'ship'
+    HIT = 'hit'
+    MISS = 'miss'
+    SUNK = 'sunk'
 
 
 @dataclass
 class CellDTO:
     state: CellState
-    x: int
-    y: int
-    ship_size: int
-    ship_orientation: str
 
 class BoardDTO():
     def __init__(self, size=5):
         self.size = size
         #Creating clear board size X size
-        self.board = [[CellDTO(CellState('E'), x, y) for y in range(size)] for x in range(size)]
+        self.board = [[CellDTO(CellState('empty')) for y in range(size)] for x in range(size)]
 
 @dataclass
 class Field:
